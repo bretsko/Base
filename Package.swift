e4c7bea30f8da11f3e920cd1d29e7fb81ed5ae5f
Oleksandr Bretsko <bretsko.oleksandr@gmail.com>
2022-06-04 02:41:16 +0300

    update

--- Package.swift
+++ Package.swift
@@ -1,51 +1,52 @@
-// swift-tools-version:5.6
+// swift-tools-version: 5.6
 
 import PackageDescription
 
 let package = Package(
     name: "Base",
     
-     platforms: [
-        .macOS(.v12), .iOS(.v15) 
+    platforms: [
+        .macOS(.v12), .iOS(.v15)
     ],
     products: [
         .library(
             name: "Base",
             targets: ["Base"]),
     ],
-    dependencies: [    
+    dependencies: [
         .package(url: "https://github.com/apple/swift-algorithms", from: "1.0.0"),
         
-        .package(
-            url: "https://github.com/apple/swift-collections.git", 
-            .upToNextMajor(from: "1.0.0") // or `.upToNextMinor
+            .package(
+                url: "https://github.com/apple/swift-collections.git",
+                    .upToNextMajor(from: "1.0.0")
             ),
         
         // .package(url: "../../Quick/Quick", from: "1.0.0"),
         // .package(url: "../../Quick/Nimble", from: "1.0.0"),
-        .package(url: "https://github.com/bretsko/Quick", from:"1.0.0"),
-        .package(url: "https://github.com/bretsko/Nimble", from:"1.0.0"),
-
+        
         // QuickObjCRuntime build fails on ios and mac
-        // .package(url: "https://github.com/Quick/Quick", from: "3.0.0"),
-        // .package(url: "https://github.com/Quick/Nimble", from: "9.0.0-rc.1"),
-
+        //        .package(url: "https://github.com/Quick/Quick", from: "5.0.0"),
+        //        .package(url: "https://github.com/Quick/Nimble", from: "10.0.0"),
+        
     ],
     targets: [
-
+        
         .target(name: "Base", dependencies: [
             .product(name: "Algorithms", package: "swift-algorithms"),
             .product(name: "Collections", package: "swift-collections")
         ]),
-
         
         // .target(
         //     name: "VariantKit", // not used currently
         //     dependencies: ["Base"]),
-
-         .testTarget(
-             name: "BaseTests",
-             dependencies: ["Base",
-                            "Quick", "Nimble"]),
+        
+            // .testTarget(
+            //     name: "BaseTests",
+            //     dependencies: [
+            //     "Base",
+            //     .product(name: "Quick", package: "Quick"),
+            //     .product(name: "NimbleLib", package: "Quick")
+            //     ]
+            //     ),
     ]
 )
